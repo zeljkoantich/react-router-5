0cea2d9d32c4f7937a5da86dc6cb7bb3ff5a80da
zeljko_a <zeljko.antich@gmail.com>
2020-07-10 17:27:30 +0200

    fix

--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import AppDiv from './App';
+import App from './App';
 import { BrowserRouter } from 'react-router-dom';
 import './style.scss';
 
 ReactDOM.render(
   <BrowserRouter>
-    <AppDiv />
+    <App />
   </BrowserRouter>,
   document.getElementById('root')
 );
